bc32e0627d90c74648aa3512d15d8abb79322d83
max k <maxkuhn@gmail.com>
2022-09-20 11:06:11 -0700

    Set lights to flash every 125 ms

--- src/components/NoteIndividual.jsx
+++ src/components/NoteIndividual.jsx
@@ -1,21 +1,36 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 
 export default function NoteIndividual(props) {
   const [isOn, setIsOn] = useState(false);
-  const [lightClass, setLightClass] = useState("note-light off");
+  const [currentNote, setCurrentNote] = useState(props.currentNote);
+  const [lightClass, setLightClass] = useState(() => { 
+    if (props.currentNote == props.rowID) return "note-light on";
+    return "note-light off";
+  });
+  // const noteFunction = useRef();
 
-  useEffect(() => {
-    if (props.rowID.toString() === props.currentNote) {
-      setLightClass("note-light on");
-    }
-  }, [props])
+  // useEffect(() => {
+  //   noteFunction.current = checkNote;
+  //   noteFunction.current();
+  // }, [checkNote, props]);
 
+
+
+  // const checkNote = function() {
+  //   if (props.rowID.toString() === props.currentNote) {
+  //     setLightClass("note-light on");
+  //     setTestText("BOOP!");
+  //   };
+  //   if (props.rowID.toString() !== props.currentNote) {
+  //     setLightClass("note-light off");
+  //     setTestText("");
+  //   };
+  // };
 
   return (
     <div className="note-individual">
-      <div className={lightClass}></div>
+      <div className={lightClass} key={props.currentNote}></div>
       {props.rowID + 1}
-      current note is {props.currentNote}
     </div>
   )
 }
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -3,26 +3,35 @@ import NoteIndividual from './NoteIndividual';
 
 export default function Notes(props) {
   const [ currentNote, setCurrentNote ] = useState(0);
-  const [ hasStarted, setHasStarted ] = useState(false); 
+  const [ hasStarted, setHasStarted ] = useState(false);
   
-  // REPLACE USEEFFECT WITH CUSTOM USEINTERVAL HOOK - SEE https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentNote((prev) => {
+        if(prev > 14) return 0;
+        if(prev < 15) return prev + 1;
+      })
+    }, 125);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
   // useEffect(() => {
-  const startClock = function() {  
-    if (hasStarted) return;
-    setInterval(() => {
-      if (currentNote > 14) setCurrentNote(0);
-      if (currentNote < 15) setCurrentNote(prev => prev + 1);
-    }, 250);
-    setHasStarted(true);
-  }
+  //   setInterval(() => {
+  //     setCurrentNote(prev => {
+  //       if (prev > 14) return 0;
+  //       if (prev < 15) return prev + 1;
+  //     })
+  //   }, 500);
   // }, [])
 
 
 
   return (
-    <div className="notes-container" onClick={startClock()}>
+    <div className="notes-container">
       {Array(16).fill(1).map((el, i) =>
-      <NoteIndividual key={i} rowID={i} currentNote={currentNote}/>
+      <NoteIndividual key={i + (currentNote / 50)} rowID={i} currentNote={currentNote}/>
     )}
     </div>
   )
